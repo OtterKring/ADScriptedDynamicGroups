cd44efe2d636c9ce06afb516e08f808d478c4db0
Maximilian Otter <maximilian.otter@agrana.com>
2023-07-05 14:41:51 +0200

    bugfix

--- private/removeGroupMembers.ps1
+++ private/removeGroupMembers.ps1
@@ -2,7 +2,7 @@ function removeGroupMembers ( $Group, $Members ) {
 
     Write-Verbose ( "Removing {0} members from group {1}" -f $Members.Count, $Group.Name )
     
-    Remove-ADGroupMember $existingGroup.ObjectGuid -Members $Members -Confirm:$false
+    Remove-ADGroupMember $Group.ObjectGuid -Members $Members -Confirm:$false
     
     # reduce the MemberCount from the parent's GroupDescription variable by the amount of removed members
     $GroupDescription.MemberCount -= $Members.Count
